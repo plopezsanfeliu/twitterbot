997544b79be8d6f6f431be860e8997bec7284618
plopezsanfeliu <plopezsanfeliu@gmail.com>
2017-05-24 18:26:37 +0200

    Categories URL working.

--- notwicias/urls.py
+++ notwicias/urls.py
@@ -5,4 +5,5 @@ from notwicias import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^bot/start', views.start_bot, name='start_bot'),
+    url(r'^category/(?P<category_id>[0-9]{1})/$', views.category, name='category'),
 ]
--- notwicias/views.py
+++ notwicias/views.py
@@ -11,8 +11,12 @@ def index(request):
     return render(request, 'notwicias/index.html', {'last_tweets': last_tweets})
 
 
+def category(request, category_id):
+    category_set = Tweet.objects.filter(category=category_id).order_by('-twitter_id')[:5]
+    return render(request, 'notwicias/category.html', {'category_set': category_set, 'category_id': category_id})
+
+
 def start_bot(request):
     b = Bot()
     b.loop()
-
     return HttpResponse("Server running")
