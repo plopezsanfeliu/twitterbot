e71eed5160fd056a6d9f535c645bd295d6b5478e
plopezsanfeliu <plopezsanfeliu@gmail.com>
2017-05-24 17:04:03 +0200

    External Bot working and getting tweets properly.

--- notwicias/urls.py
+++ notwicias/urls.py
@@ -5,5 +5,4 @@ from notwicias import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^bot/start', views.start_bot, name='start_bot'),
-    url(r'^bot/stop', views.stop_bot, name='stop_bot'),
 ]
--- notwicias/views.py
+++ notwicias/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from notwicias.bot import Bot
+from notwicias.models import Tweet
 
 
 def index(request):
-    return HttpResponse("Index of Notwicias")
+    last_tweets = Tweet.objects.filter(category=0).order_by('-twitter_id')[:5]
+    return render(request, 'notwicias/index.html', {'last_tweets': last_tweets})
 
 
 def start_bot(request):
@@ -14,10 +16,3 @@ def start_bot(request):
     b.loop()
 
     return HttpResponse("Server running")
-
-
-def stop_bot(request):
-    b = Bot()
-    b.loop = False
-
-    return HttpResponse("Server stopped")
